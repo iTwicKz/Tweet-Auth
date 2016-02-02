7bd34c184c1bb72d3b88da526a10bcd60d08e07b
iTwicKz <twickes32@ufl.edu>
2016-02-02 16:14:07 -0500

    Initial Commit

---
+++ Tweet-Auth/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tweet-Auth
+//
+//  Created by Takashi Wickes on 2/2/16.
+//  Copyright © 2016 Takashi Wickes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
